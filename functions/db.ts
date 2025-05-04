1f8a18f203461c687aebcb4b470be74955a0da6b
nishoof <88177986+nishoof@users.noreply.github.com>
2025-05-03 19:04:47 -0700

    Switch to Mongoose

--- functions/db.ts
+++ functions/db.ts
@@ -1,55 +1,159 @@
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
-
-/**
- * Test represents a test taken by the user
- */
-export type Test = {
-    testName: string;
-    testScore: number;
-}
-
-/**
- * Credit represents a USF course that the user has received credit for
- * The course is the USF course name, the source_type is where the credit came from (a specific AP test, placement test, etc.)
- */
-export type Credit = {
-    course: string;
-    source: string;
-}
-
-/**
- * UserData represents the data for a user in the database
- */
-export type UserData = {
-    _id: string;
-    username: string;
-    apTests: Test[];
-    placementTests: Test[];
-    completedCourses: string[];
-    credits: Credit[];
-}
+import mongoose, { connect, Document, Model, mongo } from "mongoose";
+import { getApCredit } from "./credit-ap";
+import { getCSPlacementCredit } from "./credit-placement";
 
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 if (!uri) {
     throw new Error("Env not configured with MONGODB_URI");
 }
+await connect(uri);
 
-const client = new MongoClient(uri);
+export interface UserInterface extends Document {
+    username: string;
+    apTests: {
+        testName: string;
+        testScore: number;
+    }[];
+    placementTests: {
+        testName: string;
+        testScore: number;
+    }[];
+    completedCourses: string[];
+    credits: {
+        course: string;
+        source: string;
+    }[];
+}
 
-// Returns the placement and ap test data for the user
-export async function getUserData(username: string): Promise<UserData> {
-    try {
-        await client.connect();
-        const database = client.db("creg-db");
-        const creditCollection = database.collection("users");
+const UserSchema = new mongoose.Schema<UserInterface>({
+    username: { type: String, required: true, unique: true },
+    apTests: [
+        {
+            testName: { type: String, required: true },
+            testScore: { type: Number, required: true }
+        }
+    ],
+    placementTests: [
+        {
+            testName: { type: String, required: true },
+            testScore: { type: Number, required: true }
+        }
+    ],
+    completedCourses: [String],
+    credits: [
+        {
+            course: { type: String, required: true },
+            source: { type: String, required: true }
+        }
+    ]
+});
 
-        // Find the user in the collection
-        const user = await creditCollection.findOne({ username: username });
+// Ensure the model isn't overwritten if it already exists
+const User: Model<UserInterface> = (mongoose.models.User as Model<UserInterface>) || mongoose.model<UserInterface>("User", UserSchema);
 
-        return user as unknown as UserData;
-    } finally {
-        await client.close();   // Close the connection when done
+async function addCredit(user: (mongoose.Document<any, any, UserInterface> & UserInterface), credits: { course: string; source: string }) {
+    // Check if the user already has the credits
+    const existingCreditsCourses = user.credits.map((credit) => credit.course);
+    if (existingCreditsCourses.includes(credits.course)) {
+        console.log(`User ${user.username} already has credit for ${credits.course}`);
+        return;
     }
+
+    // Add the new credits to the user's credits array
+    user.credits.push(credits);
+    console.log(`Added credit for ${credits.course} to user ${user.username}`);
+}
+
+export async function createUser(username: string) {
+    console.log(`Creating user ${username}`);
+
+    const existingUser: UserInterface | null = await User.findOne({ username: username });
+    if (existingUser) {
+        return;
+    }
+
+    const newUser = new User({ username: username });
+    await newUser.save();
+}
+
+export async function getUserData(username: string): Promise<UserInterface | null> {
+    const user = await User.findOne({ username: username });
+    return user;
+}
+
+export async function addAPTest(username: string, test: { testName: string; testScore: number }) {
+    console.log(`Adding AP test ${test.testName} for user ${username}`);
+
+    // Find the user in the database
+    const user = await User.findOne({ username: username });
+
+    // If the user does not exist, create the user and restart the process
+    if (!user) {
+        await createUser(username);
+        return addAPTest(username, test);
+    }
+
+    // Check if an AP test with the same name already exists for the user
+    const existingTest = user.apTests.find((t) => t.testName === test.testName);
+
+    // If so, update its score
+    if (existingTest) {
+        existingTest.testScore = test.testScore;
+        const credits = getApCredit(test.testName, test.testScore);
+        if (credits.length > 0) {
+            // Add the credit to the user's credits array
+            const creditsWithSource = credits.map((c) => ({ course: c, source: test.testName }));   // proper format for the database
+            for (const credit of creditsWithSource) {
+                await addCredit(user, credit);
+            }
+        }
+        await user.save();
+        return;
+    }
+
+    // Otherwise, add the new test to the user's apTests array
+    user.apTests.push(test);
+    await user.save();
+}
+
+export async function addPlacementTest(username: string, test: { testName: string; testScore: number }) {
+    console.log(`Adding placement test ${test.testName} for user ${username}`);
+
+    // Find the user in the database
+    const user = await User.findOne({ username: username });
+
+    // If the user does not exist, create the user and restart the process
+    if (!user) {
+        await createUser(username);
+        return addPlacementTest(username, test);
+    }
+
+    // Check if a placement test with the same name already exists for the user
+    const existingTest = user.placementTests.find((t) => t.testName === test.testName);
+
+    // If so, update its score
+    if (existingTest) {
+        existingTest.testScore = test.testScore;
+
+        // Add credit for CS placement tests
+        if (test.testName === "CS Placement") {
+            const credits = getCSPlacementCredit(test.testScore);
+            if (credits.length > 0) {
+                // Add the credit to the user's credits array
+                const creditsWithSource = credits.map((c) => ({ course: c, source: test.testName }));   // proper format for the database
+                for (const credit of creditsWithSource) {
+                    await addCredit(user, credit);
+                }
+            }
+        }
+
+        await user.save();
+        return;
+    }
+
+    // Otherwise, add the new test to the user's placementTests array
+    user.placementTests.push(test);
+    await user.save();
 }
--- functions/recommender.ts
+++ functions/recommender.ts
@@ -1,26 +1,41 @@
-import { getUserData } from "./db";
+import { createUser, getUserData } from "./db";
 
-const csPathway = ["CS110", "CS111", "CS112", "CS221", "CS245"];
+const csPathway = ["CS 110", "CS 111", "CS 112", "CS 221", "CS 245"];
+
+export async function getUserDataOrMakeUser(username: string) {
+    let user = await getUserData(username);
+    if (user)
+        return user;
+
+    await createUser(username);
+    return getUserData(username);
+}
 
 export async function getCourseRecommendations(email: string) {
     const username = email.split("@")[0];
-    const userdata = await getUserData(username);
+    const userdata = await getUserDataOrMakeUser(username);
+    if (!userdata) {
+        throw new Error("User not found and could not be created.");
+    }
     const credits = userdata.credits;
 
-    if (!credits) {
+    console.log(`User ${username} has the following credits: ${credits}`);
+
+    // If the user doesn't have any credits, recommend the first course in the pathway
+    if (credits.length === 0) {
         return csPathway[0];
     }
 
-    // Go through the courses in order
-    for (const course of csPathway) {
-        // Check if the user has credit for this course already
-        if (credits.some((credit) => {
-            return credit.course === course;
-        })) continue;
+    // Convert credits to a set of just the course names
+    const creditSet = new Set(credits.map((credit) => credit.course));
 
-        // If not, then they should do this course
-        return course;
+    // Find the first course in the pathway that the user does not have credit for, then return it
+    for (const course of csPathway) {
+        if (!creditSet.has(course)) {
+            return course;
+        }
     }
 
+    // If the user has credit for all courses in the pathway, return null
     return null;
 }
