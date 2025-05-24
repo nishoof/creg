053523564c8e89b373422834160b47e80c91d729
nishoof <88177986+nishoof@users.noreply.github.com>
2025-05-23 19:30:43 -0700

    feat: major req page uses major from db

--- app/major-requirements/page.tsx
+++ app/major-requirements/page.tsx
@@ -1,45 +1,77 @@
+import { auth, authenticate } from '@/auth';
+import { getUserData } from '@/functions/db';
+import { getMajorVideo } from '@/functions/majors';
 import Link from 'next/link';
-import styles from './page.module.css';
 
-export default function MajorRequirements() {
-  const majorVideos = [
-    { name: "Accounting", url: "https://youtu.be/Yrbj5ilS9ro" },
-    { name: "Advertising", url: "https://www.youtube.com/watch?v=-xu4FN1csPw" },
-    { name: "Asian Studies", url: "https://youtu.be/CxeQB-MyO8Y" },
-    { name: "Architecture", url: "https://youtu.be/m075Kctv9Gg" },
-    { name: "Biology", url: "https://youtu.be/FK5CjHk0Tbo" },
-    { name: "Chemistry", url: "https://www.youtube.com/watch?v=3sUDzwGaPrY" },
-    { name: "Computer Science", url: "https://www.youtube.com/watch?v=Nrrz92sU314" },
-    { name: "Data Science", url: "https://youtu.be/mrinjBbrSNU" },
-    { name: "Design", url: "https://youtu.be/qAm2T9_WDdY" },
-    { name: "Engineering", url: "https://youtu.be/gKvRX_cghU8" },
-    { name: "Mathematics", url: "https://youtu.be/-TRooFEBiNc" },
-    { name: "Physics", url: "https://youtu.be/otsFJBp1yVM" },
-    { name: "Psychology", url: "https://youtu.be/un0ua791UGs" },
-    { name: "Undeclared Business", url: "https://youtu.be/RdohQKNGIPo" },
-    { name: "Undeclared Arts", url: "https://youtu.be/Ybo3qd0pijI" },
-    { name: "Undeclared Science", url: "https://youtu.be/E-nxzZ8IzfI" }
-  ];
+export default async function MajorRequirements() {
+  // Page title and description
+  const pageTitle = "Major-Specific Graduation Requirements";
+  const pageDescription = "Learn about the specific requirements and recommendations for your chosen major and see example schedules";
+
+  // Auth
+  const session = await auth();
+  const authenticatedUser = authenticate(session);
+  const loggedIn = authenticatedUser !== false;
+
+  // Handle not logged in
+  if (!loggedIn) {
+    return (
+      <div className="page">
+        <main className="main">
+          <div className="titleSection">
+            <h1>{pageTitle}</h1>
+            <p>{pageDescription}</p>
+          </div>
+
+          <p>Please log in and select your major to view this page.</p>
+        </main>
+      </div>
+    );
+  }
+
+  // Get user's major
+  const username = authenticatedUser.email.split('@')[0];
+  const userdata = await getUserData(username);
+  const major = userdata.major;
+
+  // Handle no major selected
+  if (!major) {
+    return (
+      <div className="page">
+        <main className="main">
+          <div className="titleSection">
+            <h1>{pageTitle}</h1>
+            <p>{pageDescription}</p>
+          </div>
+
+          <p>Please select your major on the <Link className="link" href="/account">Account page</Link> to view this page.</p>
+        </main>
+      </div>
+    );
+  }
+
+  // Get video to display based on major
+  const videoUrl = getMajorVideo(major);
 
   return (
     <div className="page">
       <main className="main">
         <div className="titleSection">
-          <h1>Major-Specific Graduation Requirements</h1>
-          <p>Learn about the specific requirements and recommendations for your chosen major and see example schedules</p>
+          <h1>{pageTitle}</h1>
+          <p>{pageDescription}</p>
         </div>
 
-        <h2>Select your major to watch your major-specific video:</h2>
+        <h2>{major}</h2>
 
-        <ul className={styles.majorList}>
-          {majorVideos.map(major => (
-            <li key={major.name} className={styles.majorItem}>
-              <Link href={major.url} target="_blank" rel="noopener noreferrer" className={styles.majorLink}>
-                <span className={styles.majorName}>{major.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div className="videoWrapper">
+          <iframe
+            className="video"
+            src={videoUrl}
+            title="CAS Graduation Requirements"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </main>
     </div>
   );
--- functions/majors.ts
+++ functions/majors.ts
@@ -1,7 +1,27 @@
-export const majors = [
-    "Computer Science",
+interface Major {
+    name: string;
+    videoEmbedUrl: string;
+};
+
+export const majorsInfo: Major[] = [
+    {
+        name: "Computer Science",
+        videoEmbedUrl: "https://www.youtube.com/embed/Nrrz92sU314",
+    }
 ];
+
+export const majors = majorsInfo.map(major => major.name);
 
 export function isValidMajor(major: string): boolean {
     return majors.includes(major);
+}
+
+export function getMajorVideo(majorName: string): string {
+    const major = majorsInfo.find(major => major.name === majorName);
+
+    if (!major) {
+        throw new Error(`Major ${majorName} not found`);
+    }
+
+    return major.videoEmbedUrl;
 }
